b5dc2629fba1677bcd119133f0fa24987c5ce9f5
Figlyarka <114225227+Figlyarka@users.noreply.github.com>
2023-06-22 01:02:13 +0500

    Update Serviceman.cs

--- Army/Serviceman.cs
+++ Army/Serviceman.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Army
 {
-    // в задании 14 мы сделали класс Serviceman IComparable
-    public class Serviceman : IComparable<Serviceman>
+
+    public class Serviceman 
     {
         public string Name { get; set; }
         public string Surname { get; set; }
@@ -30,8 +30,8 @@ namespace Army
             NumMillitaryTicket = numMillitaryTicket;
         }
 
-        // В 13 задании мы сделали метод GetInfo() виртуальным.
-        public virtual string GetInfo()
+
+        public  string GetInfo()
         {
             var typeofservice = "";
             if (TypeOfService == TypeOfService.Contract)
